52d4149cecb6c93219e2d850106a1a6bb5d02034
flb <floatlibai@gmail.com>
2026-07-08 10:48:06 +0800

    refactor 30b-npu script & remove gpu script
    Signed-off-by: flb <floatlibai@gmail.com>

--- scripts/run-qwen3-30B-A3B-npu.sh
+++ scripts/run-qwen3-30B-A3B-npu.sh
@@ -1,93 +1,140 @@
-export SLIME_SCRIPT_TRAIN_BACKEND=megatron
-export PYTHONPATH="/root/Megatron-Bridge/src:/root/Megatron-LM/:$PYTHONPATH"
+#!/bin/bash
+
+# for rerun the task
+pkill -9 -f '[v]llm serve|VLL[M]::'
+pkill -9 -f VLLM
+sleep 3
+ray stop --force
+pkill -9 ray
+pkill -9 python
+sleep 3
+pkill -9 ray
+pkill -9 python
+pkill -9 redis
+
+set -ex
+
+export PYTHONUNBUFFERED=1
 export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-export CUDA_DEVICE_MAX_CONNECTIONS=1
 export RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES=1
+export CUDA_DEVICE_MAX_CONNECTIONS=1
 export HCCL_HOST_SOCKET_PORT_RANGE=60000-60050
 export HCCL_NPU_SOCKET_PORT_RANGE=61000-61050
 export HYDRA_FULL_ERROR=1
-export MASTER_PORT=$(shuf -i 20000-65000 -n 1)  # or any free port
 export DISABLE_L2_CACHE=1
 export VLLM_ASCEND_ENABLE_NZ=0
+export VLLM_USE_AOT_COMPILE=0
+export PYTHONPATH="/root/Megatron-Bridge/src:/root/Megatron-LM/:${PYTHONPATH:-}"
 
+unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
 
-unset http_proxy
-unset https_proxy
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "${SCRIPT_DIR}/models/qwen3-30B-A3B.sh"
 
+DATA_ROOT="${DATA_ROOT:-/root}"
 
-SCRIPT_DIR="/root/vime/scripts"
-source "${SCRIPT_DIR}/models/qwen3-30B-A3B-npu.sh"
-MODEL_ROOT="${MODEL_ROOT:-/root}"
+CKPT_ARGS=(
+   --hf-checkpoint ${DATA_ROOT}/weights/Qwen3-30B-A3B/
+   --load ${DATA_ROOT}/weights/Qwen3-30B-A3B/
+   --ref-load ${DATA_ROOT}/weights/Qwen3-30B-A3B/
+   --megatron-to-hf-mode bridge
+)
 
-python /root/vime/train.py \
-  --train-backend megatron \
-  --actor-num-nodes 1 \
-  --actor-num-gpus-per-node 8 \
-  --rollout-num-gpus 8 \
-  --rollout-num-gpus-per-engine 8 \
-  ${MODEL_ARGS[@]} \
-  \
-  --hf-checkpoint ${MODEL_ROOT}/models/Qwen3-30B-A3B/ \
-  \
-  --prompt-data ${MODEL_ROOT}/datasets/dapo-math-17k/dapo-math-17k.jsonl \
-  --input-key prompt \
-  --label-key label \
-  --apply-chat-template \
-  --rollout-shuffle \
-  --rm-type deepscaler \
-  \
-  --rollout-backend vllm \
-  --vllm-weight-sync-mode native \
-  --vllm-gpu-memory-utilization 0.7 \
-  --vllm-enable-expert-parallel \
-  --vllm-enable-sleep-mode \
-  --vllm-max-model-len $((1024 * 20)) \
-  \
-  --num-rollout 300 \
-  --rollout-batch-size 32 \
-  --n-samples-per-prompt 8 \
-  --rollout-max-response-len $((1024 * 8)) \
-  --rollout-temperature 1.0 \
-  --global-batch-size 256 \
-  --balance-data \
-  \
-  --advantage-estimator grpo \
-  --kl-loss-coef 0.0 \
-  --kl-loss-type low_var_kl \
-  --entropy-coef 0.0 \
-  --eps-clip 0.2 \
-  --eps-clip-high 0.28 \
-  \
-  --optimizer adam \
-  --lr 1e-6 \
-  --lr-decay-style constant \
-  --weight-decay 0.1 \
-  --adam-beta1 0.9 \
-  --adam-beta2 0.98 \
-  --optimizer-cpu-offload \
-  --overlap-cpu-optimizer-d2h-h2d \
-  --use-precision-aware-optimizer \
-  \
-  --tensor-model-parallel-size 4 \
-  --sequence-parallel \
-  --pipeline-model-parallel-size 1 \
-  --context-parallel-size 1 \
-  --expert-model-parallel-size 8 \
-  --expert-tensor-parallel-size 1 \
-  --recompute-granularity full \
-  --recompute-method uniform \
-  --recompute-num-layers 1 \
-  --use-dynamic-batch-size \
-  --max-tokens-per-gpu 20480 \
-  --load ${MODEL_ROOT}/models/Qwen3-30B-A3B/ \
-  --megatron-to-hf-mode bridge \
-  \
-  --attention-dropout 0.0 \
-  --hidden-dropout 0.0 \
-  --accumulate-allreduce-grads-in-fp32 \
-  --attention-softmax-in-fp32 \
-  --attention-backend flash \
-  --use-flash-attn \
-  --no-gradient-accumulation-fusion \
-  \
-  --train-memory-margin-bytes 2147483648
+ROLLOUT_ARGS=(
+   --prompt-data ${DATA_ROOT}/datasets/dapo-math-17k/dapo-math-17k.jsonl
+   --input-key prompt
+   --label-key label
+   --apply-chat-template
+   --rollout-shuffle
+   --rm-type deepscaler
+   --num-rollout 3000
+   --rollout-batch-size 32
+   --n-samples-per-prompt 8
+   --rollout-max-response-len $((1024 * 8))
+   --rollout-temperature 1
+   --global-batch-size 256
+   --balance-data
+)
+
+EVAL_ARGS=(
+   --eval-interval 20
+   --eval-prompt-data aime ${DATA_ROOT}/datasets/aime-2024/aime-2024.jsonl
+   --n-samples-per-eval-prompt 16
+   --eval-max-response-len 16384
+   --eval-top-p 1
+)
+
+PERF_ARGS=(
+   --tensor-model-parallel-size 4
+   --sequence-parallel
+   --pipeline-model-parallel-size 1
+   --context-parallel-size 1
+   --expert-model-parallel-size 8
+   --expert-tensor-parallel-size 1
+
+   --recompute-granularity full
+   --recompute-method uniform
+   --recompute-num-layers 1
+
+   --use-dynamic-batch-size
+   --max-tokens-per-gpu 20480
+)
+
+GRPO_ARGS=(
+   --advantage-estimator grpo
+   --use-kl-loss
+   --kl-loss-coef 0.00
+   --kl-loss-type low_var_kl
+   --entropy-coef 0.00
+   --eps-clip 0.2
+   --eps-clip-high 0.28
+)
+
+OPTIMIZER_ARGS=(
+   --optimizer adam
+   --lr 1e-6
+   --lr-decay-style constant
+   --weight-decay 0.1
+   --adam-beta1 0.9
+   --adam-beta2 0.98
+
+   --optimizer-cpu-offload
+   --overlap-cpu-optimizer-d2h-h2d
+   --use-precision-aware-optimizer
+)
+
+VLLM_ARGS=(
+   --rollout-num-gpus-per-engine 4
+   --vllm-gpu-memory-utilization 0.7
+   --vllm-cudagraph-capture-sizes 1 2 4 8 $(seq 16 8 256)
+)
+
+MISC_ARGS=(
+   --attention-dropout 0.0
+   --hidden-dropout 0.0
+   --accumulate-allreduce-grads-in-fp32
+   --attention-softmax-in-fp32
+   --attention-backend flash
+
+   --use-flash-attn
+   --no-gradient-accumulation-fusion
+)
+
+export MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+ray start --head --node-ip-address ${MASTER_ADDR} --disable-usage-stats --dashboard-host=0.0.0.0 --dashboard-port=8265
+
+ray job submit --address="http://127.0.0.1:8265" \
+   -- python3 train_async.py \
+   --actor-num-nodes 1 \
+   --actor-num-gpus-per-node 8 \
+   --rollout-num-gpus 8 \
+   ${MODEL_ARGS[@]} \
+   ${CKPT_ARGS[@]} \
+   ${ROLLOUT_ARGS[@]} \
+   ${OPTIMIZER_ARGS[@]} \
+   ${GRPO_ARGS[@]} \
+   ${PERF_ARGS[@]} \
+   ${EVAL_ARGS[@]} \
+   ${VLLM_ARGS[@]} \
+   ${MISC_ARGS[@]}
